99a6fabb59d04937fe286688017cf61d303a1ec8
0dotexchange <0dotexchange@users.noreply.github.com>
2025-11-03 06:09:39 +0900

    refactor: wire all modules through lib.rs

--- program/src/entrypoint.rs
+++ program/src/entrypoint.rs
@@ -2,12 +2,14 @@ use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
 };
 
+use crate::processor::Processor;
+
 entrypoint!(process_instruction);
 
 fn process_instruction(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
-    _instruction_data: &[u8],
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
 ) -> ProgramResult {
-    Ok(())
+    Processor::process(program_id, accounts, instruction_data)
 }
