99049d1f34630eb4e0e33be1377c9f240d6bf4e1
0dotexchange <0dotexchange@users.noreply.github.com>
2025-12-24 11:15:23 +0900

    feat: add SDK public exports

---
+++ sdk/src/index.ts
@@ -0,0 +1,6 @@
+export { ZeroClient } from './zero';
+export { ZeroConfig, DEFAULT_CONFIG } from './types';
+export * from './types';
+export * from './instructions';
+export * from './accounts';
+export { findDaoAddress, findProposalAddress, findAgentAddress, findTreasuryAddress, findVoteRecordAddress } from './utils';
